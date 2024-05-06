b8fc4a8dc01047a67cdef96b85f6f42c5d02d71d
AJRed <redchief94@gmail.com>
2024-05-06 19:18:39 -0400

    Redesign

--- tubedl.py
+++ tubedl.py
@@ -1,40 +1,43 @@
- 
 from pytube import YouTube
-import ffmpeg
 import tkinter as tk
 
-def getText():
-    url = urlbox.get()
-    yt = YouTube(url)
-    yt.streams.first().download()
 
+class GUI:
+	def __init__(self):
+		self.url = ""
+		self.window = tk.Tk()
+		self.frame_main = tk.Frame(master=self.window)
+		self.lbl_title = tk.Label(master=self.frame_main, text="Youtube Downloader")
+		self.lbl_info = tk.Label(master=self.frame_main, text="Downloads highest quality\nprogressive stream available.")
+		self.btn_get = tk.Button(master=self.frame_main, text="Get", command=self._on_download_button_clicked)
+		self.fld_url = tk.Entry(master=self.frame_main)
+		
+		self.frame_main.grid(row=1, column=1)
+		self.lbl_title.grid(row=1, column=1)
+		self.lbl_info.grid(row=2, column=1)
+		self.btn_get.grid(row=1, column=0)
+		self.fld_url.grid(row=1, column=1)
 
+	def mainloop(self):
+		self.window.mainloop()
 
-window = tk.Tk()
+	def _on_download_button_clicked(self):
+		self.url = self.fld_url.get()
+		self.lbl_info["fg"] = "#7A7A00"
+		self.lbl_info["text"] = "Download in progress..."
+		self.window.update()
+		try:
+			yt = YouTube(self.url)
+			yt.streams.first().download()
+			self.lbl_info["fg"] = "#00AA00"
+			self.lbl_info["text"] = "Download Complete!"
+			self.window.update()
+		except Exception as e:
+			self.lbl_info["fg"] = "#FF0000"
+			self.lbl_info["text"] = "Download Error: Check URL."
+			print(e)
 
-frame_main = tk.Frame(master=window)
-frame_main.grid(row=1, column=1)
-
-title = tk.Label(master=frame_main, text="YouTube Download")
-title.grid(row=0, column=1)
-
-urlbox = tk.Entry(master=frame_main)
-urlbox.grid(row=1, column=1)
-
-btn_get = tk.Button(master=frame_main, text="get", command=getText)
-btn_get.grid(row=1, column=0)
-
-lbl_desc = tk.Label(master=frame_main, text="Downloads highest quality\n progressive stream available")
-lbl_desc.grid(row=2, column=1)
-
-
-
-
-window.mainloop()
-
-
-#video = yt.streams.get_by_itag('251').download()
-
-
-
+if (__name__ == "__main__"):
+	app = GUI()
+	app.mainloop()
 

e4c3991e206e24348844cd9d423bb634524f4d3b
AJRed <redchief94@gmail.com>
2024-05-06 17:36:09 -0700

    Add Windows installers

---
+++ install.bat
@@ -0,0 +1,10 @@
+
+echo Creating Python virtual environment.
+
+python -m venv .\env
+
+echo Installing dependentcies
+
+python -m pip install --requirement requirements.txt
+
+echo Installation complete.
